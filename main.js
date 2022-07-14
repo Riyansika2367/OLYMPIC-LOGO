5e529dac10a8584c99660fffb26287c66bbd062e
Riyansika2367 <107189711+Riyansika2367@users.noreply.github.com>
2022-07-14 20:49:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+canvas - document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.storkeStyle = "grey";
+ctx.lineWidth = 4;
+ctx.rect(150,143,430,200);
+ctx.storkeStyle();
+
+ctx.beginPath();
+ctx.storkeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(350,210,40,0,2 = Math.PI);
+ctx.storkeStyle();
+
+ctx.beginPath();
+ctx.storkeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arct(250,210,40,0,2 = Math.PI);
+ctx.storkeStyle();
+
+ctx.beginPath();
+ctx.storkeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(350,210,40,0,2 = Math.PI);
+ctx.storkeStyle();
+
+ctx.beginPath();
+ctx.storkeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(350,210,40,0,2 = Math.PI);
+ctx.storkeStyle();
+
+ctx.beginPath();
+ctx.storkeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(350,210,40,0,2 = Math.PI);
+ctx.storkeStyle();
+
+ctx.beginPath();
+ctx.storkeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(350,210,40,0,2 = Math.PI);
+ctx.storkeStyle();
